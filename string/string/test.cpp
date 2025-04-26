06e861344b036a7678882ca9e7b7ecbfdced6e62
Coke <1760198676@qq.com>
2025-04-26 19:37:01 +0800

    1

--- string/string/test.cpp
+++ string/string/test.cpp
@@ -50,35 +50,109 @@ using namespace std;
 //	return 0;
 //}
 
-// 范围for 
+//// 范围for 
+//int main() {
+//	int arr[5] = { 0,1,2,3,4 };
+//	// C++ 98
+//	for (size_t i = 0; i < sizeof(arr) / sizeof(int); i++)
+//	{
+//		cout << arr[i] << " " ;
+//	}
+//	cout << endl;
+//	// 修改数据
+//	for (size_t i = 0; i < sizeof(arr) / sizeof(int); i++)
+//	{
+//		cout << arr[i] * 2 << " ";
+//	}
+//
+//	// C++ 11
+//	cout << endl;
+//	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+//	cout << endl;
+//	for (auto& e : arr) cout << e << " ";
+//	for (auto& e : arr) e *= 2;
+//	cout << endl;
+//	for (auto& e : arr) cout << e << " ";
+//
+//	cout << endl;
+//
+//	string str("hello, world!");
+//	for (auto str : str) cout << str;
+//
+//	return 0;
+//
+//}
+
+// string类的常用接口
+
+// //1. 拷贝构造之类
+//void TestS1() {
+//     初始化字符串对象 s0
+//    string s0("Initing string.");
+//
+//     创建一个空字符串对象 s1
+//    string s1;
+//
+//     创建一个string的空类 s11
+//    string s11();
+//
+//     使用 s0 拷贝构造，s2 内容和 s0 完全一样
+//    string s2(s0);
+//
+//     从 s0 的位置 5 开始，创建一个新的字符串 s3，内容是 s0 从索引 5 开始到末尾的字符串
+//    string s3(s0, 5);
+//
+//    cout << s0 << endl;
+//    cout << s1 << endl;
+//     是一个类 cout<<s11会报错
+//    cout << s11 << endl;
+//    cout << s2 << endl;
+//    cout << s3 << endl;
+//}
+
+//// 遍历操作
+//void TestS2() {
+//    string s("hello world");
+//    // 1. 下标+[]
+//    cout << "operator[]:";
+//    for (size_t i = 0; i < s.size(); i++)
+//    {
+//        cout << s[i];
+//    }
+//    cout << endl;
+//
+//    // 2.迭代器begin+end
+//    // iterator begin();const_iterator begin() const; 返回迭代器指向的字符串字符的第一个位置
+//    // iterator end();const_iterator end() const;     返回迭代器指向的字符串字符最后位置的下一个位置
+//    // 迭代器范围[begin,end+1)
+//    cout << "迭代器begin+end :";
+//
+//    string::iterator it = s.begin(); // 可以将it理解成指针，但他不是指针
+//    while (it != s.end()) {
+//        cout << *it;
+//        ++it;
+//    }
+//    cout << endl;
+//
+//    // 3. 反向迭代器rbegin+rend
+//    // string::reverse_iterator rit = s.rbegin();
+//    // C++11可以用auto自动识别类型
+//    cout << "反向迭代器rbegin+rend :";
+//    auto rit = s.rbegin();
+//    while (rit != s.rend()) {
+//        cout << *rit;
+//        ++rit;
+//    }
+//    cout << endl;
+//
+//    // 4. 范围for
+//    cout << "范围for: ";
+//    for (auto ch : s) cout << ch;
+//}
+//
 int main() {
-	int arr[5] = { 0,1,2,3,4 };
-	// C++ 98
-	for (size_t i = 0; i < sizeof(arr) / sizeof(int); i++)
-	{
-		cout << arr[i] << " " ;
-	}
-	cout << endl;
-	// 修改数据
-	for (size_t i = 0; i < sizeof(arr) / sizeof(int); i++)
-	{
-		cout << arr[i] * 2 << " ";
-	}
+   // TestS1();
+   // TestS2();
 
-	// C++ 11
-	cout << endl;
-	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-	cout << endl;
-	for (auto& e : arr) cout << e << " ";
-	for (auto& e : arr) e *= 2;
-	cout << endl;
-	for (auto& e : arr) cout << e << " ";
-
-	cout << endl;
-
-	string str("hello, world!");
-	for (auto str : str) cout << str;
-
-	return 0;
-
+    return 0;
 }
